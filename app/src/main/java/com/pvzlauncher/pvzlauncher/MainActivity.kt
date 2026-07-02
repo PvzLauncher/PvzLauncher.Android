97933fd6bfc9779a9ca726d3d330e29e48486f86
Xiaowang0229 <wangjiaxu20120229@163.com>
2026-07-02 12:31:44 +0800

    Update

--- app/src/main/java/com/pvzlauncher/pvzlauncher/MainActivity.kt
+++ app/src/main/java/com/pvzlauncher/pvzlauncher/MainActivity.kt
@@ -4,8 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,6 +30,34 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import com.pvzlauncher.pvzlauncher.ui.theme.PvzLauncherAndroidTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BugReport
+import androidx.compose.material.icons.filled.Download
+import androidx.compose.material.icons.filled.FileOpen
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.ManageSearch
+import androidx.compose.material.icons.filled.QuestionMark
+import androidx.compose.material.icons.filled.Rocket
+import androidx.compose.material.icons.filled.RocketLaunch
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Update
+import androidx.compose.material.icons.filled.Upgrade
+import androidx.compose.material.icons.filled.VideogameAsset
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+const val APP_VERSION = "1.0.0-alpha.1";
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,10 +71,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
 @PreviewScreenSizes
 @Composable
 fun PvzLauncherAndroidApp() {
-    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HomePage) }
 
     NavigationSuiteScaffold(
         navigationSuiteItems = {
@@ -45,8 +84,8 @@ fun PvzLauncherAndroidApp() {
                 item(
                     icon = {
                         Icon(
-                            painterResource(it.icon),
-                            contentDescription = it.label
+                            it.icon,
+                            it.label
                         )
                     },
                     label = { Text(it.label) },
@@ -57,21 +96,150 @@ fun PvzLauncherAndroidApp() {
         }
     ) {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
+            val pageModifier = Modifier.padding(innerPadding)
+
+            // 用 when 表达式根据当前选中的 destination 切换不同的 UI
+            when (currentDestination) {
+                AppDestinations.HomePage -> {
+                    Box(modifier = Modifier.fillMaxSize()){
+                        Image(painter = painterResource(id=R.drawable.ic_apptitle_zh),"123",
+                            alignment = Alignment.TopCenter, modifier = Modifier.padding(40.dp).fillMaxWidth())
+                        Column(modifier = Modifier.align(Alignment.BottomCenter).padding(40.dp), horizontalAlignment = Alignment.CenterHorizontally)
+                        {
+                            Button(onClick = {
+
+                            })
+                            {
+                                Column()
+                                {
+                                    Row(modifier = Modifier.padding(10.dp), verticalAlignment = Alignment.CenterVertically){
+
+                                        Icon(imageVector = Icons.Default.RocketLaunch,"", modifier = Modifier.padding(5.dp).size(24.dp))
+                                        Text("启动游戏",modifier = Modifier.padding(5.dp), fontWeight = FontWeight.Bold, fontSize = 18.sp)
+                                    }
+
+                                }
+                            }
+                            Row()
+                            {
+                                Text("当前游戏：" , fontSize = 14.sp)
+                                Text("CurrentGame", fontSize = 14.sp, fontWeight = FontWeight.Bold)
+                            }
+                        }
+                    }
+                }
+                AppDestinations.ManagePage -> {
+                    Column(){
+
+                    }
+                }
+                AppDestinations.DownloadPage -> {
+                    Column(){
+
+                    }
+                }
+                AppDestinations.SettingPage -> {
+                    Column(){
+
+                    }
+                }
+                AppDestinations.AboutPage -> {
+                    Column(Modifier.fillMaxSize().padding(10.dp)){
+                        Card(Modifier.fillMaxWidth().padding(5.dp)){
+                            Column(Modifier.padding(10.dp).fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally)
+                            {
+
+                                    Image(
+                                        painter = painterResource(id=R.drawable.ic_appicon_vector),
+                                        contentDescription = "AppIcon",
+                                        modifier = Modifier.size(200.dp),
+
+                                        )
+                                    Row()
+                                    {
+                                        Text("PvzLauncher for Android", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                                        Text("Post-Reset", fontSize = 10.sp,modifier= Modifier.padding(2.dp))
+                                    }
+                                Row(verticalAlignment = Alignment.CenterVertically)
+                                {
+                                    Row()
+                                    {
+                                        Text("版本：",fontSize=14.sp, fontWeight = FontWeight.Normal, modifier = Modifier.padding(2.dp))
+                                        Text(text = APP_VERSION, fontSize = 14.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(2.dp))
+                                    }
+                                    Button(onClick = {
+
+                                    }, modifier = Modifier.padding(5.dp))
+                                    {
+                                        Icon(imageVector = Icons.Default.Upgrade,"检测更新")
+                                        Text("检测更新", modifier = Modifier.padding(5.dp))
+                                    }
+                                }
+                                Row()
+                                {
+                                    Button(onClick = {
+
+                                    }, modifier = Modifier.padding(5.dp))
+                                    {
+
+                                        Text("反馈漏洞")
+                                    }
+                                    Button(onClick = {
+
+                                    }, modifier = Modifier.padding(5.dp))
+                                    {
+
+                                        Text("许可协议")
+                                    }
+                                    Button(onClick = {
+
+                                    }, modifier = Modifier.padding(5.dp))
+                                    {
+
+                                        Text("常见问题")
+                                    }
+
+
+                                }
+
+
+                            }
+
+                        }
+
+                        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(5.dp).fillMaxWidth())
+                        {
+                            Text("开发者", fontWeight = FontWeight.Bold, modifier = Modifier.padding(2.dp))
+                            Text("Xiaowang0229 - 主要开发人员", modifier = Modifier.padding(2.dp))
+                            Text("版权方", fontWeight = FontWeight.Bold, modifier = Modifier.padding(2.dp))
+                            Text("ishuamouren - 启动器版权方", modifier = Modifier.padding(2.dp))
+                            Text("开发者与 PopCap Games、Electronic Arts 没有任何关联，不获得其任何授权或背书。", modifier = Modifier.padding(2.dp))
+                            Text("贡献者", fontWeight = FontWeight.Bold, modifier = Modifier.padding(2.dp))
+                            Text("衷心感谢支持PvzLauncher的每一名用户！", modifier = Modifier.padding(2.dp))
+
+
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
+
+
 enum class AppDestinations(
     val label: String,
-    val icon: Int,
+    val icon: ImageVector,
 ) {
-    HOME("Home", R.drawable.ic_home),
-    FAVORITES("Favorites", R.drawable.ic_favorite),
-    PROFILE("Profile", R.drawable.ic_account_box),
+    //HOME("主页", R.drawable.ic_home),
+    HomePage(label="启动",icon=Icons.Default.Rocket),
+    ManagePage(label="管理",icon=Icons.Default.VideogameAsset),
+    DownloadPage(label="下载",icon=Icons.Default.Download),
+    SettingPage(label="设置",icon=Icons.Default.Settings),
+    AboutPage(label="关于",icon=Icons.Default.Info)
+
+
 }
 
 @Composable
--- app/src/main/java/com/pvzlauncher/pvzlauncher/ui/theme/Theme.kt
+++ app/src/main/java/com/pvzlauncher/pvzlauncher/ui/theme/Theme.kt
@@ -10,17 +10,58 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.pvzlauncher.pvzlauncher.R
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFFFFFFFF),
+    secondary = Color(0xFF0288D1),
+    tertiary = Color(0xFF01579B)
+)
+
+val appfontfamily = FontFamily(
+    Font(resId = R.font.appfont_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.appfont_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.appfont_thin, weight = FontWeight.Thin),
+    Font(resId = R.font.appfont_black, weight = FontWeight.Black),
+    Font(resId = R.font.appfont_light, weight = FontWeight.Light),
+    Font(resId = R.font.appfont_medium, weight = FontWeight.Medium),
+)
+
+val AppTypography = Typography()
+
+// 3. 核心：用你的字体，强行覆盖系统默认的所有字号样式
+val GlobalTypography = Typography(
+    displayLarge = AppTypography.displayLarge.copy(fontFamily = appfontfamily),
+    displayMedium = AppTypography.displayMedium.copy(fontFamily = appfontfamily),
+    displaySmall = AppTypography.displaySmall.copy(fontFamily = appfontfamily),
+
+    headlineLarge = AppTypography.headlineLarge.copy(fontFamily = appfontfamily),
+    headlineMedium = AppTypography.headlineMedium.copy(fontFamily = appfontfamily),
+    headlineSmall = AppTypography.headlineSmall.copy(fontFamily = appfontfamily),
+
+    titleLarge = AppTypography.titleLarge.copy(fontFamily = appfontfamily),
+    titleMedium = AppTypography.titleMedium.copy(fontFamily = appfontfamily),
+    titleSmall = AppTypography.titleSmall.copy(fontFamily = appfontfamily),
+
+    bodyLarge = AppTypography.bodyLarge.copy(fontFamily = appfontfamily),
+    bodyMedium = AppTypography.bodyMedium.copy(fontFamily = appfontfamily),
+    bodySmall = AppTypography.bodySmall.copy(fontFamily = appfontfamily),
+
+    labelLarge = AppTypography.labelLarge.copy(fontFamily = appfontfamily),
+    labelMedium = AppTypography.labelMedium.copy(fontFamily = appfontfamily),
+    labelSmall = AppTypography.labelSmall.copy(fontFamily = appfontfamily)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFF29B6F6),
+    secondary = Color(0xFF0288D1),
+    tertiary = Color(0xFF01579B)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -38,6 +79,7 @@ fun PvzLauncherAndroidTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
+
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -52,7 +94,7 @@ fun PvzLauncherAndroidTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = GlobalTypography,
         content = content
     )
 }
