2b50b9e90d35c5a5a6116f7db2561eb9d9af7742
Xiaowang0229 <wangjiaxu20120229@163.com>
2026-07-03 23:46:47 +0800

    Update:更新Alpha02.部分01

--- app/src/main/java/com/pvzlauncher/pvzlauncher/MainActivity.kt
+++ app/src/main/java/com/pvzlauncher/pvzlauncher/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.pvzlauncher.pvzlauncher
 
+import android.content.Intent
+import android.content.res.Resources
+import android.net.Uri
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.widget.ImageButton
+import android.widget.ToggleButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,6 +14,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +22,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -32,6 +40,7 @@ import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import com.pvzlauncher.pvzlauncher.ui.theme.PvzLauncherAndroidTheme
 import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowRightAlt
 import androidx.compose.material.icons.filled.BugReport
 import androidx.compose.material.icons.filled.Download
 import androidx.compose.material.icons.filled.FileOpen
@@ -45,19 +54,24 @@ import androidx.compose.material.icons.filled.RocketLaunch
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Update
 import androidx.compose.material.icons.filled.Upgrade
+import androidx.compose.material.icons.filled.Upload
 import androidx.compose.material.icons.filled.VideogameAsset
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.util.Objects
 
-const val APP_VERSION = "1.0.0-alpha.1";
+const val APP_VERSION = "1.0.0-alpha.2";
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -129,22 +143,36 @@ fun PvzLauncherAndroidApp() {
                     }
                 }
                 AppDestinations.ManagePage -> {
-                    Column(){
-
+                    Column(modifier = Modifier.padding(10.dp,35.dp)){
+                        Text("管理", fontWeight = FontWeight.Bold,modifier = Modifier.padding(5.dp), fontSize = 24.sp)
+                        XW_GameInformationCard(painterResource(R.drawable.ic_appicon_vector),"Title","Description",null)
                     }
                 }
                 AppDestinations.DownloadPage -> {
-                    Column(){
-
+                    Column(modifier = Modifier.padding(10.dp,35.dp)){
+                        Text("下载", fontWeight = FontWeight.Bold,modifier = Modifier.padding(5.dp), fontSize = 24.sp)
+                        XW_GameInformationCard(painterResource(R.drawable.ic_appicon_vector),"Title","Description",null)
                     }
                 }
                 AppDestinations.SettingPage -> {
-                    Column(){
-
+                    Column(modifier = Modifier.padding(10.dp,35.dp)){
+                        Text("设置", fontWeight = FontWeight.Bold,modifier = Modifier.padding(5.dp,5.dp,5.dp,10.dp), fontSize = 24.sp)
+                        Column(modifier = Modifier.padding(10.dp,2.dp))
+                        {
+                            Text("主题设置", fontWeight = FontWeight.Bold,modifier = Modifier.padding(0.dp), fontSize = 18.sp)
+                            XW_Switch("自动切换主题",modifier = Modifier.padding(0.dp))
+                            XW_Switch("启用深色模式",modifier = Modifier.padding(0.dp))
+                        }
+                        Column(modifier = Modifier.padding(10.dp,2.dp))
+                        {
+                            Text("标题设置", fontWeight = FontWeight.Bold,modifier = Modifier.padding(0.dp), fontSize = 18.sp)
+                            XW_Switch("启用英文版标题",modifier = Modifier.padding(0.dp))
+                        }
                     }
                 }
                 AppDestinations.AboutPage -> {
-                    Column(Modifier.fillMaxSize().padding(10.dp)){
+                    Column(modifier = Modifier.padding(10.dp,35.dp)){
+                        Text("关于", fontWeight = FontWeight.Bold,modifier = Modifier.padding(5.dp), fontSize = 24.sp)
                         Card(Modifier.fillMaxWidth().padding(5.dp)){
                             Column(Modifier.padding(10.dp).fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally)
                             {
@@ -169,30 +197,48 @@ fun PvzLauncherAndroidApp() {
                                     }
                                     Button(onClick = {
 
-                                    }, modifier = Modifier.padding(5.dp))
+                                    }, modifier = Modifier.padding(5.dp).size(48.dp),contentPadding = PaddingValues(0.dp),
+                                        shape = CircleShape
+                                    )
                                     {
-                                        Icon(imageVector = Icons.Default.Upgrade,"检测更新")
-                                        Text("检测更新", modifier = Modifier.padding(5.dp))
+
+                                        Icon(imageVector = Icons.Default.Upload,"检测更新", modifier = Modifier.size(32.dp))
+
                                     }
                                 }
                                 Row()
                                 {
+                                    val context = LocalContext.current
                                     Button(onClick = {
-
+                                        val intent = Intent(
+                                            Intent.ACTION_VIEW,
+                                            Uri.parse("https://www.google.com")
+                                        )
+                                        context.startActivity(intent)
                                     }, modifier = Modifier.padding(5.dp))
                                     {
 
                                         Text("反馈漏洞")
                                     }
-                                    Button(onClick = {
 
+                                    Button(onClick = {
+                                        val intent = Intent(
+                                            Intent.ACTION_VIEW,
+                                            Uri.parse("https://www.google.com")
+                                        )
+                                        context.startActivity(intent)
                                     }, modifier = Modifier.padding(5.dp))
                                     {
 
                                         Text("许可协议")
                                     }
-                                    Button(onClick = {
 
+                                    Button(onClick = {
+                                        val intent = Intent(
+                                            Intent.ACTION_VIEW,
+                                            Uri.parse("https://www.google.com")
+                                        )
+                                        context.startActivity(intent)
                                     }, modifier = Modifier.padding(5.dp))
                                     {
 
@@ -242,18 +288,3 @@ enum class AppDestinations(
 
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PvzLauncherAndroidTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/pvzlauncher/pvzlauncher/XW_Controls.kt
+++ app/src/main/java/com/pvzlauncher/pvzlauncher/XW_Controls.kt
@@ -1,2 +1,76 @@
 package com.pvzlauncher.pvzlauncher
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowRightAlt
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.util.Objects
+
+@Composable
+public fun XW_Switch(text : String,modifier: Modifier) {
+    var checked by remember { mutableStateOf(false) }
+
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
+        Text(text,modifier = Modifier.padding(10.dp,5.dp), fontSize = 16.sp)
+
+        Switch(
+            checked = checked,
+            onCheckedChange = { checked = it }
+        )
+    }
+}
+
+@Composable
+public fun XW_GameInformationCard(headImage : Painter,gametitle : String,gamedescription : String,args : Objects?)
+{
+    return Card(modifier = Modifier.padding(5.dp).fillMaxWidth())
+    {
+        Box(modifier = Modifier.padding(5.dp).fillMaxWidth())
+        {
+            Row(modifier = Modifier.align(Alignment.CenterStart), verticalAlignment = Alignment.CenterVertically)
+            {
+                Image(painter = headImage,"", modifier = Modifier.padding(5.dp).size(64.dp))
+                Column(modifier = Modifier.padding(15.dp), horizontalAlignment = Alignment.CenterHorizontally)
+                {
+                    Text(gametitle,modifier = Modifier.padding(2.dp), fontWeight = FontWeight.Bold, fontSize = 18.sp)
+                    Text(gamedescription,modifier = Modifier.padding(2.dp), fontSize = 14.sp)
+                }
+            }
+            Button(onClick = {
+
+            }, modifier = Modifier.align(Alignment.CenterEnd).padding(5.dp).size(48.dp),contentPadding = PaddingValues(0.dp),
+                shape = CircleShape
+            )
+            {
+
+                Icon(imageVector = Icons.Default.ArrowRightAlt,"检测更新", modifier = Modifier.size(32.dp))
+
+            }
+
+
+        }
+    }
+}
